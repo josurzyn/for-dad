34242e61249027706d44961950fd4a6e65ca888b
Jo Surzyn <josurzyn@gmail.com>
2020-03-22 10:26:11 +0100

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,15 @@
+var gulp = require('gulp');
+var bs = require('browser-sync').create();
+var reload = bs.reload;
+
+gulp.task('serve', function() {
+  bs.init({
+    server: {
+      baseDir: '.'
+    },
+    notify: false
+  });
+
+  gulp.watch('*.html').on('change', reload);
+  gulp.watch('css/*.css').on('change', reload);
+})
